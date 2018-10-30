d66603647e9c25b90fe6587784e6e3397e6e6ac3
William <williamspruyt@hotmail.com>
2018-10-30 13:54:18 +0000

    Updates

---
+++ precache-manifest.7d37caf3074f48275c47fffe1c3b3b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/UnitCircle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d5d0a36fd0778741755",
+    "url": "/UnitCircle/static/js/runtime~main.2d5d0a36.js"
+  },
+  {
+    "revision": "dcab9e94c262c0b082c4",
+    "url": "/UnitCircle/static/js/main.dcab9e94.chunk.js"
+  },
+  {
+    "revision": "54e2511bb533a7446d9e",
+    "url": "/UnitCircle/static/js/1.54e2511b.chunk.js"
+  },
+  {
+    "revision": "dcab9e94c262c0b082c4",
+    "url": "/UnitCircle/static/css/main.0f1f96ed.chunk.css"
+  },
+  {
+    "revision": "9fb1cb97ef9ce8ed9074a2abd2b911fb",
+    "url": "/UnitCircle/index.html"
+  }
+];
