e1f089feeec289c72f3833891ab312a6d21baa2e
William <williamspruyt@hotmail.com>
2018-10-30 16:25:00 +0000

    Updates

--- precache-manifest.e2c9d5b3625afac8adf9c06421cded21.js
+++ precache-manifest.e2c9d5b3625afac8adf9c06421cded21.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/UnitCircle/static/js/runtime~main.2d5d0a36.js"
   },
   {
-    "revision": "e41b45eef4e8bb099b11",
-    "url": "/UnitCircle/static/js/main.e41b45ee.chunk.js"
+    "revision": "8d50a926cbfb472397da",
+    "url": "/UnitCircle/static/js/main.8d50a926.chunk.js"
   },
   {
     "revision": "b89bd89bd0c215345705",
     "url": "/UnitCircle/static/js/1.b89bd89b.chunk.js"
   },
   {
-    "revision": "e41b45eef4e8bb099b11",
+    "revision": "8d50a926cbfb472397da",
     "url": "/UnitCircle/static/css/main.0f1f96ed.chunk.css"
   },
   {
-    "revision": "e41f0c6bd68fc7021cce01df10119cf6",
+    "revision": "33f0c89f4247a7c998f2a9d5ed512037",
     "url": "/UnitCircle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/UnitCircle/precache-manifest.57174f9ab09c3c92d139c4e07cb09c24.js"
+  "/UnitCircle/precache-manifest.e2c9d5b3625afac8adf9c06421cded21.js"
 );
 
 workbox.clientsClaim();
